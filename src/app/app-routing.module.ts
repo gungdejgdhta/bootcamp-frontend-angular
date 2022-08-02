a09ab956ded79d869fde84cde0acffb463683105
Anak Agung Gede Putra Jagadhita <gungdehita@gmail.com>
2022-08-02 12:48:55 +0700

    push day 7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ProfileComponent} from "./profile/profile.component";
+import {CountComponent} from "./count/count.component";
+import {ValidationComponent} from "./validation/validation.component";
 
-const routes: Routes = [{
+const routes: Routes = [
+  {
   path: 'profile',
   component: ProfileComponent
-}
+  },
+  {
+    path: 'count',
+    component: CountComponent
+  },
+  {
+    path: 'validation',
+    component: ValidationComponent
+  }
 ];
 
 @NgModule({
